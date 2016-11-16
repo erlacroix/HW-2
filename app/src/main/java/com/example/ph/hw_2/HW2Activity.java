a817124a294e9fba5216ca343e99add6c771f746
Yu Tang <Yu Tang>
2016-11-16 09:11:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ph/hw_2/HW2Activity.java
@@ -0,0 +1,13 @@
+package com.example.ph.hw_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HW2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hw2);
+    }
+}
